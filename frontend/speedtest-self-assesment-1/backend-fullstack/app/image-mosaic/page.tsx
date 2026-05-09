4b5840d47929adc811e591e7a2e1c215917a947f
ankit723 <varanasiartist.omg@gmail.com>
2026-05-09 14:53:51 +0530

    hello

--- frontend/speedtest-self-assesment-1/backend-fullstack/app/image-mosaic/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-export default function Page(){
-  return(
-    <div className="w-screen h=screen flex justify-center items-center">
-      
-    </div>
-  )
-}
--- frontend/speedtest-self-assesment-1/backend-fullstack/app/recurring-calendar/page.tsx
+++ frontend/speedtest-self-assesment-1/backend-fullstack/app/recurring-calendar/page.tsx
@@ -23,28 +23,31 @@ export default function Page(){
     const firstDay = getFirstDay(year, month);
     console.log("firstDay: ", firstDay)
     return(
-        <div className="w-screen h-screen flex justify-center items-center p-10">
+        <div className="w-screen h-screen flex flex-col justify-center items-center p-40">
+            <div className="">
+                <p className="text-xl font-bold">Month: {month}</p>
+            </div>
             <div className="w-full h-full grid grid-cols-7 grid-rows-7">
                 {["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"].map(day=>(
-                    <div key={day} className="border border-blue-400 flex justify-center items-center">
+                    <div key={day} className="border border-blue-400 flex justify-center items-center hover:bg-blue-300/50 cursor-pointer">
                         {day}
                     </div>
                 ))}
                 {
                     Array.from({length: firstDay}).map((d, i)=>(
-                        <div key={i} className="border border-blue-400 flex justify-center items-center"></div>
+                        <div key={i} className="border border-blue-400 flex justify-center items-center hover:bg-blue-300/50 cursor-pointer"></div>
                     ))
                 }
                 {
                     daysArray.map((day) => (
-                        <div key={day} className="border flex justify-center items-center">
+                        <div key={day} className="border flex justify-center items-center hover:bg-blue-300/50 cursor-pointer">
                             {day}
                         </div>
                     ))
                 }
                 {
                     Array.from({length: daysArray.length+firstDay}).map((day, i)=>(
-                        <div key={i} className="border border-blue-400 flex justify-center items-center"></div>
+                        <div key={i} className="border border-blue-400 flex justify-center items-center hover:bg-blue-300/50 cursor-pointer"></div>
                     ))
                 }
 
